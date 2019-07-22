f1887a6b69c319c03c0349104fcde0c83afe2cc0
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-07-22 15:27:32 -0700

    Finished Guess the Flag App.

--- Guess The Flag(HWS)/Guess The Flag(HWS)/ViewController.swift
+++ Guess The Flag(HWS)/Guess The Flag(HWS)/ViewController.swift
@@ -17,8 +17,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
-    
-    
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,16 +50,57 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.green.cgColor
         button3.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0).cgColor
         
+        // askQuestion(action: nil)
         askQuestion()
+
     }
     
-    func askQuestion() {
+    //func askQuestion(action: UIAlertAction! )
+    func askQuestion(action: UIAlertAction! = nil) {
+        
+        // add a shuffle
+        countries.shuffle()
+        
+        // create randomizer
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+    
+        title = countries[correctAnswer].uppercased()
+        navigationController?.title = title
+        
+        print(title!)
+        print(correctAnswer)
 
     }
 
-
+    // connect all the buttons to the IBACtion and use Tags
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Incorrect"
+            score -= 1
+        }
+    
+        // UIALERT
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style:.default, handler: askQuestion))
+    
+        present(ac, animated: true)
+    }
 }
 
+
+// NOTES:
+
+// UIALERT is cool
+// Pay attention to the errors
+// Title in navigation bar doesn't really make sense... but ok...
