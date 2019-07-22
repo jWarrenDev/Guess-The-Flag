9c4a8e67cc2ce32331fb8935737d06d8426fefac
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-07-22 13:26:51 -0700

    Added Project and design layout

--- Guess The Flag(HWS)/Guess The Flag(HWS)/ViewController.swift
+++ Guess The Flag(HWS)/Guess The Flag(HWS)/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
