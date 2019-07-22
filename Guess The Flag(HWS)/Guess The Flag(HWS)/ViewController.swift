97444c8fb62e3892d4b0ca869da501ccafbf4a2e
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-07-22 13:48:39 -0700

    Made basic functionality and design

--- Guess The Flag(HWS)/Guess The Flag(HWS)/ViewController.swift
+++ Guess The Flag(HWS)/Guess The Flag(HWS)/ViewController.swift
@@ -15,9 +15,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // why couldn't we just make a for loop to grab the name of the file.
+        // I guess writing the countries out makes sense.
+        
+        countries += [
+                      "estonia",
+                      "france",
+                      "germany",
+                      "ireland",
+                      "italy",
+                      "monaco",
+                      "nigeria",
+                      "poland",
+                      "russia",
+                      "spain",
+                      "uk",
+                      "us"
+                     ]
+
+        // Everything is backed by a CA Layer (low Level). Uses CoreGraphics color. UIColor is higher level
+        
+        button1.layer.borderWidth = 3
+        button2.layer.borderWidth = 3
+        button3.layer.borderWidth = 3
+
+        button1.layer.borderColor = UIColor.purple.cgColor
+        button2.layer.borderColor = UIColor.green.cgColor
+        button3.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0).cgColor
+        
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+
     }
 
 
